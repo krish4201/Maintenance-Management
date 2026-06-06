0d6ce8ec54ca774211a1fb035b40633d39d08c96
krish4201 <rkkramesh2001@gmail.com>
2026-06-07 00:30:45 +0530

    '2.0.5'

--- srv/handlers/equipment-service.js
+++ srv/handlers/equipment-service.js
@@ -1,4 +1,5 @@
 const cds = require('@sap/cds')
+const { getUserRole } = require('../lib/user-role')
 
 module.exports = cds.service.impl(async function () {
 
@@ -8,6 +9,38 @@ module.exports = cds.service.impl(async function () {
     this.on('READ', 'Equipments', async req => {
 
         return equipmentSrv.run(req.query)
+
+    })
+
+    this.before('CREATE', 'Equipments', async req => {
+
+        const role =
+            await getUserRole(
+                req.user.id,
+                req.user
+            )
+
+        if(role !== 'Planner'){
+
+            req.reject(
+                403,
+                'Only Planner'
+            )
+
+        }
+
+    })
+
+    this.on('CREATE', 'Equipments', async req => {
+
+        const { ZC_MASTER_EQUIPMENT } =
+            equipmentSrv.entities
+
+        return equipmentSrv.run(
+            INSERT
+                .into(ZC_MASTER_EQUIPMENT)
+                .entries(req.data)
+        )
 
     })
 
